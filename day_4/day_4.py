97714001b12270adc3c8cb00b74e2cb959a654b1
Justas Bauras <justasb@hotmail.co.uk>
2022-12-04 18:25:11 +0000

    Day 4 puzzle 2

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 with open("input.txt", "r", newline = "\n") as f:
     file = f.read().splitlines()
 
@@ -17,4 +19,26 @@ for line in file:
             contained_pairs += 1
 
 print(f"Number of contained pairs is {contained_pairs}")
+
+# Puzzle 2
+overlaps = 0
+
+for line in file:
+    pairs = line.split(",")
+    left_pair = pairs[0].split("-")
+    right_pair = pairs[1].split("-")
+
+    nums = np.linspace(int(left_pair[0]), int(left_pair[1]),
+                        int(left_pair[1]) - int(left_pair[0]) + 1)
+
+    for num in nums:
+        if ((round(num) >= int(right_pair[0])) 
+            and (round(num) <= int(right_pair[1]))):
+                overlaps += 1
+                break
+
+print(f"Number of any overlaps {overlaps}")
+
+
+
 
