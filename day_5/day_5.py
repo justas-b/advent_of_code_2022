b738cac613ff009e7d43f13d751101d96066dd10
Justas Bauras <justasb@hotmail.co.uk>
2022-12-05 08:50:06 +0000

    Day 5 puzzle 2

--- day_5/day_5.py
+++ day_5/day_5.py
@@ -29,4 +29,26 @@ for crate in crates:
 
 print(f"The top crates are: {top_crates}")
 
+# Puzzle 2
+new_crates = [["B", "L", "D", "T", "W", "C", "F", "M"], ["N", "B", "L"],
+          ["J", "C", "H", "T", "L", "V"], ["S", "P", "J", "W"],
+          ["Z", "S", "C", "F", "T", "L", "R"], 
+          ["W", "D", "G", "B", "H", "N", "Z"], 
+          ["F", "M", "S", "P", "V", "G", "C", "N"], 
+          ["W", "Q", "R", "J", "F", "V", "C", "Z"], ["R", "P", "M", "L", "H"]]
 
+for move in moves:
+    movement = move.split(" ")
+    crates_to_move = int(movement[1])
+    original_crate = int(movement[3]) - 1
+    new_crate = int(movement[5]) - 1
+
+    new_crates[new_crate] += new_crates[original_crate][len(new_crates[original_crate]) - crates_to_move:]
+    new_crates[original_crate] = new_crates[original_crate][0:len(new_crates[original_crate]) - crates_to_move]
+
+top_new_crates = ""
+
+for crate in new_crates:
+    top_new_crates += crate[-1]
+
+print(f"The top crates are: {top_new_crates}")
