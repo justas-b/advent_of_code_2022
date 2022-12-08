6feaf8448855e8cd016088318beb096b943fa5d7
Justas Bauras <justasb@hotmail.co.uk>
2022-12-08 12:37:58 +0000

    Day 8 puzzle 2

--- day_8/day_8.py
+++ day_8/day_8.py
@@ -25,14 +25,21 @@ def check_greater_equal_to(input: list, val: int) -> int:
     return 0 # visible
 
 
+def get_surroundings(tree_grid: list[list], row: int, col:int):
+    """Determines the surrounding trees"""
+    tree = tree_grid[row][col]
+    left_row = reversed(tree_grid[row][:col])
+    right_row = tree_grid[row][col + 1:]
+    top_col = reversed([tree_grid[row_indx][col] for row_indx in range(0, row)])
+    bottom_col = [tree_grid[row_indx][col] for row_indx in range(row + 1, len(tree_grid))]
+
+    return [left_row, right_row, top_col, bottom_col]
+
+
 def is_visible(tree_grid: list[list], row: int, col:int ) -> bool:
     """Checks whether tree is visible from ANY side"""
     tree = tree_grid[row][col]
-    left_row = tree_grid[row][:col]
-    right_row = tree_grid[row][col + 1:]
-    top_col = [tree_grid[row_indx][col] for row_indx in range(0, row)]
-    bottom_col = [tree_grid[row_indx][col] for row_indx in range(row + 1, len(tree_grid))]
-    checks = [left_row, right_row, top_col, bottom_col]
+    checks = get_surroundings(tree_grid, row, col)
     not_visible_count = 0
 
     for check in checks:
@@ -56,3 +63,41 @@ def visible_count(tree_grid: list[list]) -> int:
 visible_trees += visible_count(tree_grid)
 
 print(f"Number of visible trees is {visible_trees}")
+
+# Puzzle 2
+
+def calculate_visible_trees(input: list, val: int) -> int:
+    """Calculates number of visible trees"""
+    visible_count = 0
+    
+    for tree in input:
+        if val > tree:
+            visible_count += 1
+        else:
+            visible_count += 1
+            return visible_count
+    
+    return visible_count
+
+def calculate_scenic_score(checks: list, tree: int) -> int:
+    """Calculates the scenic score of the tree"""
+    score = 1
+
+    for check in checks:
+        score *= calculate_visible_trees(check, tree)
+
+    return score
+
+def max_scenic_score(tree_grid: list[list]) -> int:
+    max = 0
+    for row in range(1, len(tree_grid[0]) - 1):
+        for col in range(1, len(tree_grid) - 1):
+            tree = tree_grid[row][col]
+            checks = get_surroundings(tree_grid, row, col)
+            score = calculate_scenic_score(checks, tree)
+            if score > max:
+                max = score
+
+    return max
+
+print(f"Max scenic score is {max_scenic_score(tree_grid)}")
