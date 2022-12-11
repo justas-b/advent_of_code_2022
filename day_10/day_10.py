08fad21b3ef30ca5d7fa0a4a0df0ed6c46053fbe
Justas Bauras <justasb@hotmail.co.uk>
2022-12-11 21:56:56 +0000

    Day 10 puzzle 2

--- day_10/day_10.py
+++ day_10/day_10.py
@@ -1,3 +1,5 @@
+import math
+
 with open("input.txt", "r", newline="\n") as f:
     file = f.read().splitlines()
 
@@ -31,3 +33,42 @@ def signal_strength(cycles: dict) -> int:
 
 
 print(f"The sum of the signal strengths is {signal_strength(cycles)}")
+
+# Puzzle 2
+image = [[], [], [], [], [], [],]
+new_cycle = -1
+new_X = 1
+
+
+def sprite_pos(cycle: int, X: int) -> list:
+    """Gets the current position of the sprite given the cycle"""
+    row = math.floor(cycle / 40)
+    offset = 40 * row
+
+    return [(X - 1) + offset, (X) + offset, (X + 1) + offset]
+
+
+def draw_pixel(sprite: list, cycle: int) -> str:
+    """Draws a pixel if the sprite in on the same line as the cycle"""
+    if cycle in sprite:
+        return "#"
+    else:
+        return "."
+
+
+for line in file:
+    if line == "noop":
+        new_cycle += 1
+        row = math.floor(new_cycle / 40)
+        image[row].append(draw_pixel(sprite_pos(new_cycle, new_X), new_cycle))
+    else:
+        split_line = line.split(" ")
+        for i in range(0, 2):
+            new_cycle += 1
+            row = math.floor(new_cycle / 40)
+            image[row].append(draw_pixel(sprite_pos(new_cycle, new_X), new_cycle))
+
+        new_X += int(split_line[1])
+
+for row in image:
+    print("".join(row))
