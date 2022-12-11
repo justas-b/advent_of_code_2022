7b6820eb0bfd14ed8090a6dffc39f056520b67de
Justas Bauras <justasb@hotmail.co.uk>
2022-12-11 23:52:02 +0000

    Day 11 puzzle 1 start

---
+++ day_11/day_11.py
@@ -0,0 +1,67 @@
+with open("test.txt", "r", newline="\n") as f:
+    file = f.read().splitlines()
+
+# Puzzle 1
+monkeys_items = {}
+monkey_rules = {}
+monkey_checks = {}
+current_monkey = 0
+
+for line in file:
+    line = line.strip()
+    if line.startswith("Monkey"):
+        current_monkey = int(line.split(":")[0].split(" ")[-1])
+        monkeys_items[current_monkey] = []
+        monkey_rules[current_monkey] = []
+        monkey_checks[current_monkey] = 0
+    elif line.startswith("Starting"):
+        items = line.split(":")
+        monkeys_items[current_monkey] = [int(val) for val in items[-1].strip().split(", ")]
+    elif line.startswith("Operation"):
+        monkey_rules[current_monkey].append(line.split("= ")[-1])
+    elif line.startswith("Test"):
+        monkey_rules[current_monkey].append(line.split("by ")[-1])
+    elif line.startswith("If true"):
+        monkey_rules[current_monkey].append(line.split("monkey ")[-1])
+    elif line.startswith("If false"):
+        monkey_rules[current_monkey].append(line.split("monkey ")[-1])
+
+
+def do_operation(operation: str, input: int) -> int:
+    """Do the operation on the input according to the monkeys rule"""
+    output = input
+    op_list = operation.split(" ")
+    op = op_list[1]
+    operate_on = op_list[2]
+
+    if op == "+":
+        if operate_on == "old":
+            return round((output + input) / 3)
+        else:
+            return round((output + int(operate_on)) / 3)
+    elif op == "-":
+        if operate_on == "old":
+            return round((output - input) / 3)
+        else:
+            return round((output - int(operate_on)) / 3)
+    elif op == "*":
+        if operate_on == "old":
+            return round((output * input) / 3)
+        else:
+            return round((output * int(operate_on)) / 3)
+
+
+def check_divisible(worry:int, by: int, true: int, false: int) -> int:
+    """Checks whether worry level is divisible and what monkey it is thrown to"""
+    return true if (worry % 2 == 0) else false
+
+
+for monkey in monkeys_items:
+    rules = monkey_rules[monkey]
+    for i, item in enumerate(monkeys_items[monkey]):
+        monkey_checks[monkey] += 1
+        monkeys_items[monkey].remove(item)
+        worry = do_operation(rules[0], int(item))
+        throw_to = check_divisible(worry, int(rules[1]), int(rules[2]), int(rules[3]))
+
+print(monkey_checks)
